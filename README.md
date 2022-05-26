0a51004e71e77213636f39600e7fc09df1a47cf4
Doston <dostonnaboov@gmail.com>
2022-05-26 18:34:28 +0300

    updated to v1.1.0

---
+++ README.md
@@ -0,0 +1,21 @@
+# Project
+
+## Changelog
+
+- **Version 1.1.0 _(2022, May 26)_**
+
+  - 11ty added
+
+- **Version 1.0.2 _(2022, May 26)_**
+
+  - Projects layouts added
+  - Sidebar added
+  - Header updated
+
+- **Version 1.0.1 _(2022, May 24)_**
+
+  - Header added
+
+- **Version 1.0.0 _(2022, May 24)_**
+
+  - initial release of project
---
+++ src/index.js
@@ -0,0 +1,38 @@
+// Reference: https://scrimba.com/learn/spacetravel navigation section
+
+const nav = document.querySelector(".primary-nav");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  const visiblity = nav.getAttribute("data-visible");
+  if (visiblity === "false") {
+    nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else {
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
+
+// Reference: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+// Get the button
+var backToTop = document.getElementById("backToTop");
+
+// When the user scrolls down 40px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
