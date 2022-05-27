39f5e478b99527a9adac54d4e06305a95458c70d
Doston Nabotov <96492656+dostonnabotov@users.noreply.github.com>
2022-05-27 12:52:01 +0300

    updates on custom props

--- .eleventy.js
+++ .eleventy.js
@@ -1,30 +1,43 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addWatchTarget("./src/style.css");
+  eleventyConfig.addWatchTarget("./src/style.css");
 
-    eleventyConfig.addPassthroughCopy("./src/style.css");
-    eleventyConfig.addPassthroughCopy("./src/index.js");
-    eleventyConfig.addPassthroughCopy("./src/assets/");
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/index.js");
+  eleventyConfig.addPassthroughCopy("./src/assets/");
 
-    eleventyConfig.addShortcode("layout", function(layout) {
-    return `<section class="layout clearfix">
-                <div class="layout__img">
-                    <a href="${layout.link}">
-                        <img loading="lazy" src="${layout.imgUrl}" alt="${layout.imgAlt}" />
-                    </a>
-                </div>
-                <div class="layout__info flow ${layout.dark ? `bg-dark text-white` : `bg-white text-dark`}">
-                    <h3 class="fs-500">
-                        <a href="${layout.link}" class="u-hover">${layout.title}</a>
-                    </h3>
-                    <p>${layout.content}</p>
-                </div>
+  eleventyConfig.addShortcode("layout", (layout) => {
+    return `
+            <section class="layout clearfix">
+              <div class="layout__img">
+                <a href="${layout.link}">
+                  <img loading="lazy" src="${layout.imgUrl}" alt="${layout.imgAlt}" />
+                </a>
+              </div>
+              <div class="layout__info flow ${
+                layout.dark ? `bg-dark text-white` : `bg-white text-dark`
+              }">
+                <h3 class="fs-500">
+                  <a href="${layout.link}" class="u-hover">${layout.title}</a>
+                </h3>
+                <p>${layout.content}</p>
+              </div>
             </section>`;
   });
 
-    return {
-        dir: {
-            input: "src",
-            output: "public",
-        },
-    };
+  eleventyConfig.addShortcode("checklist", (checklist) => {
+    return `
+          <li>
+            <span class="fa-li">
+              <i class="fa-solid fa-check"></i>
+            </span>
+            <a href="${checklist.link}" class="u-hover">${checklist.name}</a>
+          </li>`;
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
 };
